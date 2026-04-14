0df4d0020f30b2f8592d887c5650145d514937bc
Bibhuti Saikia <Bibhutis815@gmail.com>
2026-04-14 21:18:25 +0530

    done pushing

---
+++ Day_01/script.js
@@ -0,0 +1,45 @@
+// let myName="Bibhuti saikia"
+// console.log(myName);
+
+// let num1 = "11ac";
+// let num2 = Number(num1);
+
+// console.log(typeof num1);
+// console.log(typeof num2);
+
+// console.log(num2);
+
+// let name = "ABCD";
+// let id = 7899555;
+// console.log(`My name is ${name} and id is ${id}`);
+
+// let name = "  Bibhuti Saikia";
+// let sur=" ooooooooooo"
+// console.log(name.replace("Bibhuti", "Harish"));
+
+// console.log(name);
+// console.log(name.charAt(2));
+// console.log(name.indexOf("h"));
+// console.log(name.toUpperCase());
+
+// console.log(name.concat(sur));
+
+// Nubers
+
+// let num=12.625
+// let num1=10000000
+// console.log(num.toFixed(2));
+// console.log(num.toPrecision(4));
+// console.log(num1.toLocaleString());
+// console.log(Number.MAX_SAFE_INTEGER);
+
+// Maths
+
+// let a=10
+
+// console.log(a);
+
+// console.log();
+
+
+
