3c83e09b7a58eb15940c616c97fa6f1961b6477e
Bibhuti Saikia <116287612+Bibhuti14@users.noreply.github.com>
2026-04-19 20:47:15 +0530

    comit done Date

--- Day_01/scripts_2.js
+++ Day_01/scripts_2.js
@@ -1,6 +1,6 @@
 // Math
 
-num = 4.6;
+// num = 4.6;
 // num_1 = 4.3;
 
 // console.log(Math.round(num));
@@ -9,5 +9,54 @@ num = 4.6;
 // console.log(Math.floor(num));
 // console.log(Math.floor(num_1));
 
-console.log(Math.random());
+// console.log(Math.ceil(Math.random() * 10));
 
+// let colors = ["green", "red", "orange"];
+
+// let random_col = colors[Math.floor(Math.random() * colors.length)];
+// console.log(random_col);
+
+// console.log(Math.random(array));
+// console.log(Math.floor(Math.random()*4));
+// since random give no. btw 0 and 1 so it gives a val between 0 and 4
+
+// Array
+
+// let arr = [1, 2, 3, 4, "hi", true, 9];
+// console.log(arr.unshift(12));
+// console.log(arr);
+// console.log(arr.slice(2,5));
+// console.log(arr.join("*"));
+// let arr_2 = [44, 5556];
+// arr.push(arr_2);
+// console.log(arr[7].at(1));
+// arr_3 = arr.concat(arr_2);
+// console.log(arr_3);
+
+// Date
+
+// my_date = new Date();
+// my_date1 = new Date("2026-6-12");
+// console.log(my_date1);
+
+// let d = new Date();
+// console.log(d.getFullYear());
+// console.log(d.getDay());
+// console.log(d.getTime());
+// console.log(d.toDateString());
+// console.log(d.toLocaleString());
+// console.log(d.toLocaleDateString());
+
+// let formatted = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`;
+// console.log(formatted);
+
+// let timestamp1 = Date.now();
+// console.log(timestamp1);
+// let timestamp2 = Date.now();
+// console.log(timestamp2);
+// //Time in millisec since Jan 1 1970
+// if (timestamp1 > timestamp2) {
+//   console.log("2nd is bigger");
+// } else {
+//   console.log("None");
+// }
