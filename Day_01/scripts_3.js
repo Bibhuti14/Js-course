28215af2d30004351043d4ae5128e1b9d69f7600
Bibhuti Saikia <116287612+Bibhuti14@users.noreply.github.com>
2026-04-21 21:46:53 +0530

    donr

--- Day_01/scripts_3.js
+++ Day_01/scripts_3.js
@@ -2,10 +2,13 @@
 //   name: "Bibhuti",
 //   id: 1212,
 //   age: 24,
+//   "full name": "Bibhuti saikia",
 // };
 
 // console.log(obj.name);
 // console.log(obj["name"]);
+// console.log(obj["full name"]);
+// only this can access "full name"
 // obj.age = 60;
 // console.log(obj);
 // delete obj.age;
@@ -29,7 +32,6 @@
 //   console.log(key, person[key]);
 // }
 
-
 // let obj1 = { name: "Bibhuti" };
 // let obj2 = obj1;
 
@@ -50,3 +52,81 @@
 // console.log(a); // 10 ✅ (no change)
 
 // a and b are separate copies premitive data
+
+// const mySymbol = Symbol("description");
+
+// const user = {
+//   name: "John",
+//   [mySymbol]: 123, // Symbol as a key
+// };
+
+// console.log(user[mySymbol]);//123
+// console.log(user);
+
+// let obj = {
+//   name: "Binod",
+//   class: 5,
+//   email: "bibhuti445@gail.com",
+// };
+
+// obj.name = "Rajiv";
+// console.log(obj);
+
+// Object.freeze(obj);
+
+// obj.name = "Hassan";
+// console.log(obj);
+
+// let JsUser = {
+//   email: "Bibhuti112@gmail.com",
+// };
+
+// JsUser.greetings = function () {
+//   console.log("Hello User");
+// };
+// JsUser.greetings_2 = function () {
+//   console.log(`Hello User ${this.email}`);
+// };
+
+// console.log(JsUser);
+
+// console.log(JsUser.greetings());
+// console.log(JsUser.greetings_2());
+
+// let obj1 = {
+//   1: "a",
+//   2: "b",
+// };
+// let obj2 = {
+//   3: "c",
+//   4: "d",
+// };
+
+// let obj3 = Object.assign({}, obj1, obj2);
+// console.log(obj3);
+
+// let obj4 = { ...obj1, ...obj2 };
+// console.log(obj4);
+
+// let tinder = {};
+
+// tinder.name = "Vivek";
+// tinder.email = "Vivek121@gmail.com";
+// tinder.isLoggedIn = true;
+
+// console.log(tinder);
+
+// console.log(Object.keys(tinder));
+// console.log(Object.values(tinder));
+// console.log(tinder.hasOwnProperty('isLoggedIn'));
+
+// Object destructuring
+
+// let course = new Object();
+
+// course.name = "JavaScript";
+// course.courseinstructor = "Hitesh Choudhary";
+
+// const { courseinstructor: ins } = course;
+
+// console.log(ins);
