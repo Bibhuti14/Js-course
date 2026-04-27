1ebd131a90297d8037bc07c34212f1b418451022
Bibhuti Saikia <116287612+Bibhuti14@users.noreply.github.com>
2026-04-27 20:00:36 +0530

    function

--- Day_01/function.js
+++ Day_01/function.js
@@ -12,10 +12,58 @@
 // let result = addTwo(9, 2);
 // console.log(result);
 
-function loginMessage(user) {
-  if (user === undefined) {
-    console.log("Plese give a Value");
-  }
-  return `${user} is loged in`;
-}
-console.log(loginMessage());
+// function loginMessage(user) {
+//   if (user === undefined) {
+//     return "Plese give a Value";
+//   }
+//   return `${user} is loged in`;
+// }
+// console.log(loginMessage(11));
+
+// function price(val1, val2, ...num1) {
+//   return num1;
+// }
+// console.log(price(200, 300, 400, 700));
+
+// let user = {
+//   name: "Hitesh",
+//   id: 12345,
+// };
+
+// function handleObj(anyobject) {
+//   console.log(`my name is ${anyobject.name} and id is ${anyobject.id}`);
+// }
+
+// handleObj(user);
+
+// or
+
+// handleObj({
+//   name: "Hitesh",
+//   id: 12345,
+// });
+
+// let myArrr = [100, 200, 300, 400, 500];
+
+// function handleArr(getArray) {
+//   return getArray[2];
+// }
+
+// console.log(handleArr(myArrr));
+
+// or
+
+// console.log(handleArr([100, 200, 300, 400, 500]));
+
+// This
+
+// let Tobject = {
+//   name: "Hitesh Choudhary",
+//   id: 454533,
+//   myFunc: function Greet() {
+//     console.log(`Hello ${this.name}`);
+//   },
+// };
+// Tobject.myFunc();
+
+
---
+++ Day_01/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+num = Number("hh");
+console.log(Number.isNaN(num));
+
+console.log(num);
