86483282c377e73151d5e39826490a86d3ca2cfa
Bibhuti Saikia <116287612+Bibhuti14@users.noreply.github.com>
2026-04-29 18:53:25 +0530

    flow done

---
+++ Day_01/ControlFlow.js
@@ -0,0 +1,67 @@
+// const isLoggedIn = true;
+// const loggedInGoogle = true;
+// const loggedInFacebook = false;
+
+// if (isLoggedIn && loggedInGoogle && loggedInFacebook) {
+//   console.log("Login Done");
+// } else {
+//   console.log("Please Loged In");
+// }
+
+// let month = 2;
+
+// switch (month) {
+//   case 1:
+//     console.log("January");
+//     break;
+//   case 2:
+//     console.log("February");
+//     break;
+//   default:
+//     console.log("Give valid key");
+//     break;
+// }
+
+// let month = "February";
+
+// switch (month) {
+//   case "January":
+//     console.log("January");
+//     break;
+//   case "February":
+//     console.log("February");
+//     break;
+//   default:
+//     console.log("Give valid key");
+//     break;
+// }
+
+// Detect array and Objects in conditional statements
+
+// let arr=[]
+
+// if (arr.length===0) {
+//   console.log("Empty Array");
+
+// }
+
+// let Obj = {};
+
+// if (Object.keys(Obj)) {
+//   console.log("Empty Object");
+// }
+
+// nullish coelising operator
+
+// let val1;
+
+// val1 = 5 ?? 10;
+// val1 = null ?? 5;
+// val1 = undefined ?? 5;
+// console.log(val1);
+
+// conditional stetments
+// condition?true:false
+
+// let price = 100;
+// price >= 80 ? console.log("Done") : console.log("Wrong");
