3fa94bab41ed5c542b654c9ad9e6d8aa9fd7ded1
Bibhuti Saikia <116287612+Bibhuti14@users.noreply.github.com>
2026-05-13 21:26:07 +0530

    map done

--- Day_01/iteration.js
+++ Day_01/iteration.js
@@ -35,7 +35,6 @@
 //   console.log(i);
 // }
 
-
 // let myObj = {
 //   car1: "Maruti",
 //   car2: "Maruti2",
@@ -45,3 +44,58 @@
 // for (const key in myObj) {
 //   console.log(myObj[key]);
 // }
+
+// let myname = "Bibhuti";
+// for (const i in myname) {
+//   console.log(i);
+// }
+
+// for in always print index numbers.
+
+// let newArr = [1, 2, 3, 4, 5];
+
+// newArr.forEach((val) => {
+//   console.log(val);
+// });
+
+// let arr2 = [
+//   { langName: "Java", langFile: "cs" },
+//   { langName: "cpp", langFile: "rs" },
+//   { langName: "python", langFile: "mozila" },
+// ];
+
+// arr2.forEach((item) => {
+//   console.log(item.langName);
+// });
+
+// let arr = [1, 2, 4, 5, 6, 7, 8, 9, 14];
+
+// const newArr = arr.filter((i) => i > 4);
+
+// console.log(newArr);
+
+// const books = [
+//   { title: "Book One", genre: "Fiction", publish: 1981, edition: 2004 },
+//   { title: "Book Two", genre: "Non-Fiction", publish: 1992, edition: 2008 },
+//   { title: "Book Three", genre: "History", publish: 1999, edition: 2007 },
+//   { title: "Book Four", genre: "Non-Fiction", publish: 1989, edition: 2010 },
+//   { title: "Book Five", genre: "Science", publish: 2009, edition: 2014 },
+//   { title: "Book Six", genre: "Fiction", publish: 1987, edition: 2010 },
+//   { title: "Book Seven", genre: "History", publish: 1986, edition: 1996 },
+//   { title: "Book Eight", genre: "Science", publish: 2011, edition: 2016 },
+//   { title: "Book Nine", genre: "Non-Fiction", publish: 1981, edition: 1989 },
+// ];
+
+// let selectBook = books.filter((bk) => bk.genre === "History");
+// selectBook = books.filter((bk) => bk.genre === "History" && bk.edition > 1996);
+// console.log(selectBook);
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+let newArr1 = arr
+  .map((i) => i * 10)
+  .map((i) => i + 1)
+  .filter((i) => i > 40);
+
+  console.log(newArr1);
+  
