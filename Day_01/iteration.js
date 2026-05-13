69107ddd0c58816eb7237ef10ae09fbad4952c18
Bibhuti Saikia <116287612+Bibhuti14@users.noreply.github.com>
2026-05-13 20:20:35 +0530

    aaaa

--- Day_01/iteration.js
+++ Day_01/iteration.js
@@ -5,8 +5,6 @@
 //   console.log(element);
 // }
 
-
-
 // for (let i = 0; i <= 10; i++) {
 //   console.log(`For ${i}`);
 
@@ -15,4 +13,35 @@
 //   }
 // }
 
+// let i = 0;
 
+// while (i <= 10) {
+//   console.log(i);
+//   i = i + 2;
+// }
+
+// let myArray = ["Superman", "Batman", "Flash"];
+
+// let arr = 0;
+
+// while (arr < myArray.length) {
+//   console.log(`The value is ${myArray[arr]}`);
+//   arr = arr + 1;
+// }
+
+// let myName = "Bibhuti";
+
+// for (const i of myName) {
+//   console.log(i);
+// }
+
+
+// let myObj = {
+//   car1: "Maruti",
+//   car2: "Maruti2",
+//   car3: "Maruti3",
+// };
+
+// for (const key in myObj) {
+//   console.log(myObj[key]);
+// }
