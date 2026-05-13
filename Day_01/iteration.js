ae47f64784833e8ea2ea528b59c06fe75936bd41
Bibhuti Saikia <116287612+Bibhuti14@users.noreply.github.com>
2026-05-13 19:37:52 +0530

    done

---
+++ Day_01/iteration.js
@@ -0,0 +1,18 @@
+// let array = ["a", "b", "c", "d", "e"];
+
+// for (let i = 0; i < array.length; i++) {
+//   const element = array[i];
+//   console.log(element);
+// }
+
+
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(`For ${i}`);
+
+//   for (let j = 0; j <= 10; j++) {
+//     console.log(`${i} * ${j} = ${i * j}`);
+//   }
+// }
+
+
